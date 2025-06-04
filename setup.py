49f074383dec108b0f1df6601ed54c77d3119e2c
evilafo <evi2846@gmail.com>
2025-06-04 00:46:55 +0000

    Modif2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mignonfinancequant",
-    version="0.1.0",
+    version="0.1.1",
     author="Evilafo",
     author_email="evil2846@gmail.com",
     description="Une bibliothèque Python pour les calculs de finance quantitative",
